3ada5ed8c8bee92194f073fcb4eacfc522fc8591
sergeyeremeev <13.eremeev.sv@gmail.com>
2016-06-23 18:46:04 +0300

    edit allFeeds to make test fail

--- js/app.js
+++ js/app.js
@@ -8,19 +8,6 @@
 
 // The names and URLs to all of the feeds we'd like available.
 var allFeeds = [
-    {
-        name: 'Udacity Blog',
-        url: 'http://blog.udacity.com/feed'
-    }, {
-        name: 'CSS Tricks',
-        url: 'http://css-tricks.com/feed'
-    }, {
-        name: 'HTML5 Rocks',
-        url: 'http://feeds.feedburner.com/html5rocks'
-    }, {
-        name: 'Linear Digressions',
-        url: 'http://feeds.feedburner.com/udacity-linear-digressions'
-    }
 ];
 
 /* This function starts up our application. The Google Feed
